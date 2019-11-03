d050e2ab287c64b8ca50fe604057d0da5efb5be4
ftpaul <ftpaul@gmail.com>
2019-11-03 15:33:30 +0000

    added helmet and improved css

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,9 @@ import React from "react"
 import "./layout.css"
 
 export default ({ children }) => (
-  <div style={{ margin: `0 auto`, maxWidth: 650, padding: `0 1rem` }}>
+  <div style={{ display: `flex-`, alignItems: `center`, height: `100vh`, display: `flex` }}>
+  <div style={{ margin: `0 auto`, maxWidth: 650, padding: `0 1rem`, display: `flex` }}>
     {children}
+  </div>
   </div>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,24 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
+import { Helmet } from "react-helmet"
 
 export default () => (
+  <>
+  <Helmet>
+          <meta charSet="utf-8" />
+          <title>My Title</title>
+          <link rel="canonical" href="http://mysite.com/example" />
+        </Helmet>
+
   <Layout>
-    <h1>Hello stranger!</h1>
-    <p>
-      Check out <Link to="/about/">who am I</Link>
-    </p>
+    <div class="container-image">
+    <img src="http://placekitten.com/250/250" />
+    </div>
+    <div class="container-text">
+    <h1>Paulo Teixeira</h1>
+    <p>I've been working on digital business for more than 5 years. Currently, I work at <u>Unbabel</u>. Thinking is something we are not able to do alone, that's why I like to collaborate with different people to reach the best solutions.</p>
+    </div>
   </Layout>
+  </>
 )
