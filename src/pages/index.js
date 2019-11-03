cc8099292800eb4f5f721b264b9766463f8b819d
ftpaul <ftpaul@gmail.com>
2019-11-03 23:07:15 +0000

    fix typo

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,7 +22,7 @@ const IndexPage = ({ data }) => { return (
     <h1>Paulo Teixeira</h1>
     <p>I've been working on digital business for more than 5 years. Currently, I work at <a href="https://unbabel.com">Unbabel</a>.</p>
     <p> Thinking is something we are not able to do alone, that's why I like to collaborate with different people to reach the best solutions.</p>
-    <p>Feel free to reach me out if you need help thinking by <a href="mailto:ftpaul+web@gmail.com">email</a> or on <a href="https://www.linkedin.com/in/ftpaul/">Linkdein</a>.</p>
+    <p>Feel free to reach me out if you need help thinking by <a href="mailto:ftpaul+web@gmail.com">email</a> or on <a href="https://www.linkedin.com/in/ftpaul/">LinkdeIn</a>.</p>
     </div>
   </Layout>
   </>)
