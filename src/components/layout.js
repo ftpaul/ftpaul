ecb55b136d3fe11ea0284e7cd4f7e5e6253d8a18
ftpaul <ftpaul@gmail.com>
2019-11-03 16:22:26 +0000

    css done

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,16 @@
 import React from "react"
 import "./layout.css"
+import { Helmet } from "react-helmet"
 
 export default ({ children }) => (
-  <div style={{ display: `flex-`, alignItems: `center`, height: `100vh`, display: `flex` }}>
-  <div style={{ margin: `0 auto`, maxWidth: 650, padding: `0 1rem`, display: `flex` }}>
+  <>
+  <Helmet>
+    <meta name="viewport" content="width=device-width, user-scalable=no" />
+    </Helmet>
+  <div class="container-parent" >
+  <div class="container-child" >
     {children}
   </div>
   </div>
+  </>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,23 @@
 import React from "react"
-import { Link } from "gatsby"
 import Layout from "../components/layout"
 import { Helmet } from "react-helmet"
 
 export default () => (
   <>
   <Helmet>
-          <meta charSet="utf-8" />
-          <title>My Title</title>
-          <link rel="canonical" href="http://mysite.com/example" />
-        </Helmet>
+    <meta charSet="utf-8" />
+    <title>This is the best title I could find</title>
+  </Helmet>
 
   <Layout>
     <div class="container-image">
-    <img src="http://placekitten.com/250/250" />
+    <img src="http://placekitten.com/200/200" alt="Paulo Teixeira" />
     </div>
     <div class="container-text">
     <h1>Paulo Teixeira</h1>
-    <p>I've been working on digital business for more than 5 years. Currently, I work at <u>Unbabel</u>. Thinking is something we are not able to do alone, that's why I like to collaborate with different people to reach the best solutions.</p>
+    <p>I've been working on digital business for more than 5 years. Currently, I work at <u>Unbabel</u>.</p>
+    <p> Thinking is something we are not able to do alone, that's why I like to collaborate with different people to reach the best solutions.</p>
+    <p>Feel free to reach me out by email (ftpaul [at gmail.com]) or on linkein.</p>
     </div>
   </Layout>
   </>
