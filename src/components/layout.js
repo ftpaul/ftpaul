929bd28d367e5fd7ec0faeb976ffbe5df1f15f3d
Paulo Teixeira <pauloteixeira@Paulos-MacBook-Air.local>
2019-11-04 12:29:27 +0000

    add favicon

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,12 @@
 import React from "react"
 import "./layout.css"
 import { Helmet } from "react-helmet"
+import favicon from '../images/favicon.png'
 
 export default ({ children }) => (
   <>
   <Helmet>
+    <link rel="icon" href={favicon} />
     <meta name="viewport" content="width=device-width, user-scalable=no" />
     </Helmet>
   <div class="container-parent" >
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react"
 import Layout from "../components/layout"
 import { Helmet } from "react-helmet"
 import { graphql } from 'gatsby'
-import Img from "gatsby-image"
 
 const IndexPage = ({ data }) => { return (
   <>
