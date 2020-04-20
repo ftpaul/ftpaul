a723740219a6b047bb5d228fe8e15e9d5a103c6e
Paulo Teixeira <pauloteixeira@Paulos-MBP-2.lan>
2020-04-20 22:14:35 +0100

    adding package and data

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,25 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+const resumeJson = require('./src/data/resume.json');
+
 module.exports = {
-  plugins: [`gatsby-plugin-react-helmet`, `gatsby-transformer-sharp`, `gatsby-plugin-sharp`,{ resolve: `gatsby-source-filesystem`, options: { path: `./src/images/`, quality: 100, loading: `lazy` } },]
+  plugins: [
+    `gatsby-plugin-react-helmet`, 
+    `gatsby-transformer-sharp`, 
+    `gatsby-plugin-sharp`,
+    { resolve: `gatsby-source-filesystem`, 
+      options: { 
+        path: `./src/images/`, 
+        quality: 100, 
+        loading: `lazy` 
+      } 
+    },
+    {
+      resolve: `gatsby-theme-jsonresume`,
+      options: {
+        resumeJson: { resumeJson }
+      },
+    },
+  ]
 }
