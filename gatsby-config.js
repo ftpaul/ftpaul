043b99c03fef5fa8df43c8958c4bad6f11812e14
Paulo Teixeira <pauloteixeira@Paulos-MBP-2.lan>
2020-05-30 20:17:58 +0100

    added tailwind

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`, 
     `gatsby-transformer-sharp`, 
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-postcss`,
     { resolve: `gatsby-source-filesystem`, 
       options: { 
         path: `./src/images/`, 
