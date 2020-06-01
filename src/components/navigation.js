98f2798a8dfed2eb24edb87121854e660c5e6aff
Paulo Teixeira <pauloteixeira@Paulos-MBP-2.lan>
2020-06-01 18:07:01 +0100

    adjusting classes and behavior

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,8 +4,7 @@ import { Link } from "gatsby"
 
 const Navigation = () => {
 
-
-    const [isExpanded, toggleExpansion] = useState(false);
+    const [isExpanded, toggleExpansion] = useState(false)
 
     return (
         <>
@@ -15,11 +14,11 @@ const Navigation = () => {
                     <h1 className="text-4xl text-gray-600 pb-4">Paulo Teixeira<span className="font-bold text-5xl text-yellow-600">.</span></h1>
                 </div>
                     <button className="hidden xs:display xs:flex items-center px-3 py-2 text-yellow-600 bg-organd-300 z-10" onClick={() => toggleExpansion(!isExpanded)}>
-                        <span className="text-xl text-gray-500 hover:text-yellow-600 border-b-2 border-yellow-600">Menu</span>
+                        <span className="text-xl text-gray-500 hover:text-yellow-600 border-b-2 border-yellow-600">{isExpanded ? "Close" : "Menu"}</span>
                     </button>
                     <div className={isExpanded ? `xs:block xs:visible xs:opacity-100 xs:top-0 xs:left-0 xs:absolute xs:w-screen xs:h-screen xs:bg-yellow-600 xs:ease-in-out xs:duration-1000` : `xs:hidden xs:invisible xs:opacity-0`}>
                     
-                    <ul className=" " className={isExpanded ? `xs:block text-xl xs:pt-64 xs:text-center xs:text-2l` : `text-xl flex xs:hidden`}>
+                    <ul className={isExpanded ? `xs:block text-xl xs:pt-64 xs:text-center xs:text-2l` : `text-xl flex xs:hidden`}>
                         <li className="mr-6 xs:text-gray-600 xs:text-2xl xs:mb-6">
                             <Link className="text-gray-500  hover:text-yellow-600 xs:hover:text-gray-100" activeClassName="border-b-2 border-yellow-600 xs:border-gray-100" to="/about">About</Link>
                         </li>
