2281b3f1859bc5bbf1310e63fa22cc4df7b15351
Paulo Teixeira <pauloteixeira@Paulos-MBP-2.lan>
2020-06-01 18:11:20 +0100

    updating about page name to preview

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -20,7 +20,7 @@ const Navigation = () => {
                     
                     <ul className={isExpanded ? `xs:block text-xl xs:pt-64 xs:text-center xs:text-2l` : `text-xl flex xs:hidden`}>
                         <li className="mr-6 xs:text-gray-600 xs:text-2xl xs:mb-6">
-                            <Link className="text-gray-500  hover:text-yellow-600 xs:hover:text-gray-100" activeClassName="border-b-2 border-yellow-600 xs:border-gray-100" to="/about">About</Link>
+                            <Link className="text-gray-500  hover:text-yellow-600 xs:hover:text-gray-100" activeClassName="border-b-2 border-yellow-600 xs:border-gray-100" to="/preview">About</Link>
                         </li>
                         <li className="mr-6 xs:text-gray-600 xs:text-2xl xs:mb-6">
                             <button className="text-gray-500  opacity-50 cursor-not-allowed" activeClassName="border-b-2 border-yellow-600 xs:border-gray-100" to="#" disabled={true}>Blog</button>
