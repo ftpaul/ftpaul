96f3b232fbf9e9827fd9d39ca96051517c2d2524
Paulo Teixeira <pauloteixeira@Paulos-MBP-2.lan>
2020-06-03 22:45:07 +0100

    adding 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,31 @@
+import React from "react"
+import Layout from "../components/layout"
+import { Helmet } from "react-helmet"
+import { graphql } from 'gatsby'
+
+const IndexPage = ({ data }) => { return (
+  <>
+  <Helmet>
+    <meta charSet="utf-8" />
+    <title>This is the best title I could find</title>
+  </Helmet>
+
+  <Layout>
+    <h1 className="text-2xl text-gray-600">Work in progress <span role="img" aria-label="Man Technologist">👨🏻‍💻</span></h1>
+  </Layout>
+  </>)
+}
+
+export const query = graphql`
+{
+  file(relativePath: {eq: "paulo-teixeira-square.jpg"}) {
+    childImageSharp {
+      fixed {
+        src
+      }
+    }
+  }
+}
+`
+
+export default IndexPage
