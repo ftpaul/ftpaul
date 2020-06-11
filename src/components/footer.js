789a209f6c1de9e43dd6e58ffb97875677454080
Paulo Teixeira <pauloteixeira@Paulos-MacBook-Pro-2.local>
2020-06-11 12:46:39 +0100

    adding links to the footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,10 @@ import ContentColumn from "./contentColumn"
 export default ( ) => (
 
   <ContentColumn>
-    <p className="text-sm pb-4 xs:pt-4">Made by Paulo Teixeira using Gatsby and Tailwind</p>
+    <p className="text-sm pb-4 xs:pt-4">Made by Paulo Teixeira using&nbsp;
+      <a href="https://gatsbyjs.org" className="text-yellow-600 underline hover:text-yellow-400">Gatsby</a> and&nbsp;
+      <a href="https://tailwindcss.com/" className="text-yellow-600 underline hover:text-yellow-400">Tailwind</a>.
+    </p>
   </ContentColumn>
   
 )
