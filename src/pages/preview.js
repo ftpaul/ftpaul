e0a13165e48b9264495a7233cce1167d0219af6d
Paulo Teixeira <pauloteixeira@Paulos-MacBook-Pro-2.local>
2020-06-11 12:22:54 +0100

    iterating content and testing some images

--- src/pages/preview.js
+++ src/pages/preview.js
@@ -16,11 +16,15 @@ const AboutPage = ({ data }) => { return (
     </Helmet>
   <Navigation />
   <ContentColumn>
-      <p className="text-3xl text-gray-500 pb-2">Hey there <span role="img" aria-label="Wave">👋🏽</span></p>
-      <p className="text-2xl text-gray-500 pb-2">Driven by outstanding experiences and meaningful collaborations, I am a Product Manager with a dynamic background as developer and marketer.</p>
-      {/* <p className="text-2xl text-gray-500 pb-2">Driven by outstanding experiences and meaningful collaborations, I am a Product Manager with a dynamic background as developer and marketer.</p> */}
-      <p className="text-2xl text-gray-500 pb-2">I'm currently looking for new challenges as Product Manager or Product Owner. I have 5+ years of experience working in high-growth tech organizations such as Unbabel and Landing.jobs.</p>
-      <p className="text-2xl text-gray-500 pb-2">Feel free to out to me on LinkedIn or via e-mail.</p>
+      {/* <img src="/paulo-teixeira.png" className="justify-center flex h-48 w-48" /> */}
+      {/* https://joaopedroreis.pt/image-content/post-4/webdevelopment-sessions%20(1%20of%201).jpg */}
+      <p className="text-3xl leading-tight text-gray-500 pb-4">Hey there <span role="img" aria-label="Wave">👋🏽</span></p>
+      <p className="text-2xl leading-snug text-gray-500 pb-4 text-justify">I am a Product Manager with a dynamic background as developer and marketer, with 6+ years of experience working with web technologies. I'm passion about delivering outstanding experiences and to build meaningful collaborations that allow teams to create extraordiary work. </p>
+      <img src="/image 9.jpg" alt="Paulo Teixeira - Product Manager" className="justify-center flex pb-4 max-w-md mx-auto" />
+      <p className="text-xl leading-snug text-gray-500 pb-2 text-justify">PS: I'm currently looking for new challenges as a Product Manager or Owner, if you're looking for someone to deliver great digital products get in touch with me via&nbsp; 
+        <a href="https://linkedin.com/in/ftpaul" className="text-yellow-600 underline hover:text-yellow-400">LinkedIn</a> or&nbsp;
+        <a href="mailto:ftpaul@gmail.com" className="text-yellow-600 underline hover:text-yellow-400">e-mail</a>.
+      </p>
       </ContentColumn>
       <Footer />
   </>
