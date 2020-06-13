26180242bacd0701e89d659562b7f9358b344b33
Paulo Teixeira <pauloteixeira@Paulos-MacBook-Pro-2.local>
2020-06-13 15:16:52 +0100

    adding copyright symbol to the footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,7 @@ import ContentColumn from "./contentColumn"
 export default ( ) => (
 
   <ContentColumn>
-    <p className="text-sm pb-4 xs:pt-4">Paulo Teixeira, proudly a boring person. This website uses&nbsp;
+    <p className="text-sm pb-4 xs:pt-4">&copy; Paulo Teixeira, proudly a boring person. This website uses&nbsp;
       <a href="https://gatsbyjs.org" className="text-yellow-600 underline hover:text-yellow-400">Gatsby</a> and&nbsp;
       <a href="https://tailwindcss.com/" className="text-yellow-600 underline hover:text-yellow-400">Tailwind</a>.
     </p>
