fcf14e18d060a8f0549858c1327560d456e4145e
Paulo Teixeira <pauloteixeira@Paulos-MacBook-Pro-2.local>
2020-06-13 15:13:58 +0100

    removing unwanted chars

--- src/pages/preview.js
+++ src/pages/preview.js
@@ -16,7 +16,7 @@ const AboutPage = ({ data }) => { return (
       {/* https://joaopedroreis.pt/image-content/post-4/webdevelopment-sessions%20(1%20of%201).jpg */}
       <p className="text-3xl xs:text-2xl leading-tight text-gray-500 pb-4">Hey there <span role="img" aria-label="Wave">👋🏽</span></p>
       <p className="text-2xl xs:text-xl leading-snug text-gray-500 pb-4 text-justify xs:text-left">I am a <strong>Product Manager</strong> with a dynamic background as developer and marketer, with 6+ years of experience working with web technologies. I'm passionate about delivering outstanding experiences and to build meaningful collaborations that allow teams to create extraordiary work. </p>
-us
+
       <Img fluid={data.file.childImageSharp.fluid} alt="Paulo Teixeira - Product Manager" className="h-full w-full object-cover mx-24 xs:mx-4 mb-4" />
       
       <p className="text-xl xs:text-lg leading-snug text-gray-500 pb-2 text-justify xs:text-left">PS: I'm currently open to new opportunities as a Product Manager or Owner. In case you're looking for someone to deliver high quality digital products get in touch with me via&nbsp; 
