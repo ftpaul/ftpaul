164e4bbd3939c341015e657bc3d5fdede5f402d3
Paulo Teixeira <pauloteixeira@Paulos-MacBook-Pro-2.local>
2020-06-27 13:26:17 +0100

    improced card layout: alignment, rating

--- src/pages/books.js
+++ src/pages/books.js
@@ -4,7 +4,6 @@ import Navigation from '../components/navigation'
 import SEO from '../components/seo'
 import Footer from '../components/footer'
 import ContentColumn from '../components/contentColumn'
-import { css } from "@emotion/core"
 
 
 const BooksPage = ({ data }) => { 
@@ -12,6 +11,8 @@ const BooksPage = ({ data }) => {
   const currentlyReadingBooks = data.currently_reading.edges[0].node.reviews
   const readBooks = data.read.edges[0].node.reviews.slice(0,10)
 
+  const ratingYellowStar = `<span class="text-yellow-600">★</span>`
+  const ratingGrayStar = `<span class="text-gray-400">★</span>`
 
   return (
   <>
@@ -19,26 +20,7 @@ const BooksPage = ({ data }) => {
   <Navigation />
   <ContentColumn>
       <p className="text-xl text-gray-500 pb-2">Books are my primary source of knowledge, I like to deep dive in technical books and occasional nonfiction, both in portuguese and in english. Here's you can find what I'm currently reading:</p>
-      <table className="table-auto mb-8">
-        <thead>
-          <tr>
-          <th className="px-4 py-2">Cover</th>
-          <th className="px-4 py-2">Title</th>
-          <th className="px-4 py-2">Author</th>
-          </tr>
-        </thead>
-        <tbody>
-      {currentlyReadingBooks.map((book) => (
-        
-        <tr key={book.book.id}>
-          <td className="px-4 py-2"><img src={book.book.image_url} alt={book.book.title} className="" /></td>
-          <td className="px-4 py-2"><a href={book.book.link} target="_blank" rel="noreferrer">{book.book.title}</a></td>
-          <td className="px-4 py-2">{book.book.authors[0].name}</td>
-          </tr>
-        
-        ))}
-        </tbody>
-    </table> 
+      
         <hr />
 
         <div className="grid grid-cols-2 xs:grid-cols-1 gap-2 xs:gap-4">
@@ -65,27 +47,27 @@ const BooksPage = ({ data }) => {
         <hr />
         <br />
         <p className="mt-4 text-xl text-gray-500 pb-2 ">These are the last 10 books I've read:</p>
-        <table className="table-auto mb-8">
-        <thead>
-        <tr>
-          <th className="px-4 py-2">Cover</th>
-          <th className="px-4 py-2">Title</th>
-          <th className="px-4 py-2">Author</th>
-          <th className="px-4 py-2">Rating</th>
-          </tr>
-        </thead>
-        <tbody>
-          {readBooks.map((review) => (
-          <tr key={review.book.id}>
-          <td className="px-4 py-2"><img src={review.book.image_url} alt={review.book.title} className="" /></td>
-          <td className="px-4 py-2"><a href={review.book.link} target="_blank" rel="noreferrer">{cleanBookTitle(review.book.title)}</a></td>
-          <td className="px-4 py-2">{review.book.authors[0].name}</td>
-          <td className="px-4 py-2">{review.rating}/5</td>
-          </tr>
-          ))}
-          </tbody>
-        </table> 
-        <p className="text-xl text-gray-500 pb-2 ">You can follow my reviews and reading on <a className="text-yellow-600 underline hover:text-yellow-400" href="https://www.goodreads.com/ftpaul">my Goodreads account</a>.</p>
+
+        <div className="grid grid-cols-2 xs:grid-cols-1 gap-3 xs:gap-4">
+        {readBooks.map((review) => (
+          <a href={review.book.link} target="_blank" rel="noreferrer" className="bg-gray-100 w-full flex hover:shadow-lg mr-2" css={{height:`145px`}} key={review.book.id}>
+            <div className="lg:w-24 flex-none bg-cover text-center overflow-hidden sm:max-w-sm sm:inline-block" >
+            <img className="object-none object-center" css={{height:`145px`,width:`98px`}} src={review.book.image_url} alt={review.book.title}  /> 
+            </div>
+            <div className="w-full p-3 inline-grid">
+              <div className="mb-8">
+                <div className="text-gray-900 text-lg mb-2">{cleanBookTitle(review.book.title)}</div>
+              </div>
+              <div className="flex justify-between text-sm">
+                  <p className="text-gray-900 leading-none">{review.book.authors[0].name}</p>
+                  <p dangerouslySetInnerHTML={{__html: (ratingYellowStar.repeat(review.rating)+ratingGrayStar.repeat(5-review.rating))}}></p>
+              </div>
+            </div>
+          </a>
+        ))}
+        </div>
+
+        <p className="text-xl text-gray-500 py-2 ">You can follow my reviews and reading on <a className="text-yellow-600 underline hover:text-yellow-400" href="https://www.goodreads.com/ftpaul">my Goodreads account</a>.</p>
         </ContentColumn>
         <Footer />
   </>
