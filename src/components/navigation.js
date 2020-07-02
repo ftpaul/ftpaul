44467fef01da23ef2633c8b4b06e02bd38beb497
Paulo Teixeira <ftpaul@gmail.com>
2020-07-02 17:20:42 +0100

    highlighted navigation

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -27,7 +27,7 @@ const Navigation = () => {
                             <button className="text-gray-500  opacity-50 cursor-not-allowed" activeClassName="border-b-2 border-yellow-600 xs:border-gray-100" to="#" disabled={true}>Blog</button>
                         </li> */}
                         <li className="mr-6 xs:text-gray-600 xs:text-2xl xs:mb-6">
-                            <Link className="text-gray-500  hover:text-yellow-600 xs:hover:text-gray-100" activeClassName="border-b-2 border-yellow-600 xs:border-gray-100" to="/writing/">Writing</Link>
+                            <Link className="text-gray-500  hover:text-yellow-600 xs:hover:text-gray-100" activeClassName="border-b-2 border-yellow-600 xs:border-gray-100" partiallyActive={true} to="/writing/">Writing</Link>
                         </li>
                         <li className="mr-6 xs:text-gray-600 xs:text-2xl xs:mb-6">
                             <Link className="text-gray-500  hover:text-yellow-600 xs:hover:text-gray-100" activeClassName="border-b-2 border-yellow-600 xs:border-gray-100" to="/books/">Bookshelf</Link>
