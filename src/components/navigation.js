e663b5a3236b9bba8b1a2878c87e5577d3dc0317
Paulo Teixeira <ftpaul@gmail.com>
2020-07-03 15:09:21 +0100

    adding writing to the navigation

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -23,9 +23,9 @@ const Navigation = () => {
                         <li className="mr-6 xs:text-gray-600 xs:text-2xl xs:mb-6">
                             <Link className="text-gray-500  hover:text-yellow-600 xs:hover:text-gray-100" activeClassName="border-b-2 border-yellow-600 xs:border-gray-100" to="/">About</Link>
                         </li>
-                        {/* <li className="mr-6 xs:text-gray-600 xs:text-2xl xs:mb-6">
+                        <li className="mr-6 xs:text-gray-600 xs:text-2xl xs:mb-6">
                             <Link className="text-gray-500  hover:text-yellow-600 xs:hover:text-gray-100" activeClassName="border-b-2 border-yellow-600 xs:border-gray-100" partiallyActive={true} to="/writing/">Writing</Link>
-                        </li> */}
+                        </li>
                         <li className="mr-6 xs:text-gray-600 xs:text-2xl xs:mb-6">
                             <Link className="text-gray-500  hover:text-yellow-600 xs:hover:text-gray-100" activeClassName="border-b-2 border-yellow-600 xs:border-gray-100" to="/books/">Bookshelf</Link>
                         </li>
