807291cdc727b546260f60def96f0cc028c769e0
Paulo Teixeira <ftpaul@gmail.com>
2020-07-03 10:53:00 +0100

    added box shadow to index image

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ const AboutPage = ({ data }) => { return (
       <p className="text-3xl xs:text-2xl leading-tight text-gray-500 pb-4">Hey there <span role="img" aria-label="Wave">👋🏽</span></p>
       <p className="text-2xl xs:text-xl leading-snug text-gray-500 pb-4 text-justify xs:text-left">I am Paulo Teixeira, a <strong>Product Manager</strong> with a dynamic background as a developer and in digital marketing, with 6+ years of experience working with digital products. I'm passionate about delivering outstanding experiences and to build meaningful collaborations that allow teams to create extraordiary work. </p>
 
-      <Img fluid={data.file.childImageSharp.fluid} alt="Paulo Teixeira - Product Manager" className="h-full w-full object-cover mx-24 xs:mx-4 mb-4" />
+      <Img fluid={data.file.childImageSharp.fluid} alt="Paulo Teixeira - Product Manager" className="h-full w-full object-cover mx-24 xs:mx-4 mb-4" css={{boxShadow: `10px 10px 0px 0px #F8B500;`}} />
       
       <p className="text-xl xs:text-lg leading-snug text-gray-500 pb-2 text-justify xs:text-left">PS: I'm currently open to new opportunities as a Product Manager or Owner. In case you're looking for someone to deliver high-quality digital products get in touch with me via&nbsp; 
         <a href="https://linkedin.com/in/ftpaul" className="text-yellow-600 underline hover:text-yellow-400">LinkedIn</a> or&nbsp;
