662667175c4d70a6a07bfdbb72e064dfea679f34
Paulo Teixeira <ftpaul@gmail.com>
2020-07-06 12:05:46 +0100

    adding image post to writing listening

--- src/pages/writing.js
+++ src/pages/writing.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 import Navigation from '../components/navigation'
 import SEO from '../components/seo'
 import Footer from '../components/footer'
@@ -15,12 +15,16 @@ const WritingPage = ({ data }) => { return (
     
 
       {data.allMarkdownRemark.edges.map(article => (
-        <div key={article.node.frontmatter.title} className="mt-12 mb-48">
+        <div key={article.node.frontmatter.title} className="mt-6 mb-24">
           <Link to={`/writing`+article.node.frontmatter.slug}  className="text-gray-500 ">
             <h1 className="text-3xl xs:text-2xl leading-tight text-gray-500 pb-4 hover:text-yellow-600" >{article.node.frontmatter.title}</h1>
           </Link>
+          {article.node.frontmatter.image && (
+            <Link to={`/writing`+article.node.frontmatter.slug}  className="text-gray-500 ">
+              <img src={article.node.frontmatter.image} alt={article.node.frontmatter.title} className="mb-4"  />
+            </Link>)}
           <p className="text-base leading-tight text-gray-500 pb-4" >{article.node.excerpt}</p>
-          <Link to={`/writing`+article.node.frontmatter.slug} className="text-gray-500 hover:text-yellow-600 pb-4">Read article &rarr;</Link>
+          <Link to={`/writing`+article.node.frontmatter.slug} className="text-yellow-600 underline hover:text-yellow-400 text-lg pb-4">Read article &rarr;</Link>
         </div>
       ))}
       </ContentColumn>
@@ -41,6 +45,7 @@ export const query = graphql`
         frontmatter {
           slug
           title
+          image
         }
         excerpt
       }
