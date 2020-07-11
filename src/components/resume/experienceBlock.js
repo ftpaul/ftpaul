447847a77b91a512e6bd3d8de6427a44e8a061a2
Paulo Teixeira <ftpaul@gmail.com>
2020-07-11 22:44:39 +0100

    mobile adjustments

--- src/components/resume/experienceBlock.js
+++ src/components/resume/experienceBlock.js
@@ -6,8 +6,8 @@ export default ({ job }) => (
         <img src={job.logo} alt={job.company} className="my-1 mr-2 w-12 h-12 xs:w-10 xs:h-10 align-top inline-block" />
         <p className="text-base pt-1 xs:pl-12 inline-block xs:block xs:absolute xs:-mt-16 xs:pt-4">
           <span className="font-bold">{job.role}</span> at {job.company}
-          <span className="xs:hidden"><br/></span>
-          <span className="hidden xs:block"> ‧&nbsp;</span> 
+          <span className="block xs:hidden"><br/></span>
+          <span className="hidden xs:inline-block">&nbsp;‧&nbsp;</span>
           <span className="text-base text-gray-300 ">{job.start_date} - {job.end_date}</span>
         </p>
       </div>
