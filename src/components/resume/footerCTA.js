6fddc5f2524cbe22dea69a0d97c036fccab795bd
Paulo Teixeira <ftpaul@gmail.com>
2020-07-11 22:15:53 +0100

    upgrading styles and data

---
+++ src/components/resume/footerCTA.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default ( ) => (
+
+<div className="fixed bottom-0 w-full bg-yellow-600 text-gray-500 py-4">
+    <p className="text-xl text-center">
+        <a className="bg-gray-500 text-white hover:bg-white hover:text-gray-500 py-2 px-4 border border-gray-500 hover:border-white rounded" 
+            target="_blank" rel="noreferrer"  
+            href="https://drive.google.com/file/d/1PuPYO0u5tYwWTIbTCtEbKX_ArNKQf2CV/view?usp=sharing">
+            Download full CV (PDF)
+        </a>
+    </p>
+</div>
+  
+)
---
+++ src/components/yellowTopBar.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default ( ) => (
+
+    <div className="w-screen h-2 bg-yellow-600 absolute t-0 l-0"></div>
+  
+)
