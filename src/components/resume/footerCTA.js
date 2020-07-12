a7c83ff0155236426a489e81308a82a08ecc4e8a
Paulo Teixeira <ftpaul@gmail.com>
2020-07-12 10:49:36 +0100

    ading image effect on hover

--- src/components/resume/footerCTA.js
+++ src/components/resume/footerCTA.js
@@ -4,7 +4,7 @@ export default ( ) => (
 
 <div className="fixed bottom-0 w-full bg-yellow-600 text-gray-500 py-4">
     <p className="text-xl text-center">
-        <a className="bg-gray-500 text-white hover:bg-white hover:text-gray-500 py-2 px-4 border border-gray-500 hover:border-white rounded" 
+        <a className="hover:bg-gray-500 hover:text-white bg-white text-gray-500 py-2 px-4 border hover:border-gray-500 border-white rounded transition duration-500" 
             target="_blank" rel="noreferrer"  
             href="https://drive.google.com/file/d/1PuPYO0u5tYwWTIbTCtEbKX_ArNKQf2CV/view?usp=sharing">
             Download full CV (PDF)
