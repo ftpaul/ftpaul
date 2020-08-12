70bde1b4104e492340cc3698217606639342345c
Paulo Teixeira <ftpaul@gmail.com>
2020-08-12 14:18:23 +0100

    creating components for the index page, improving hero

---
+++ src/components/about/resumeCTA.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { Link } from 'gatsby'
+
+
+export default () => (
+
+    <div className="flex xs:flex-col-reverse flex-wrap w-full mx-auto pt-8 xs:pt-0 px-2 bg-yellow-600">
+    <div className="w-1/2 lg:w-1/2 sm:1/2 xs:w-full pt-4 pr-1 xs:pr-2 ">
+      <img src="/paulo-teixeira-resume-screenshot.png" alt="Paulo Teixeira | ftpaul.io" className="w-96 mx-auto z-20" />
+    </div>
+    <div className="w-1/2 lg:w-1/2 sm:1/2 xs:w-full pt-4 pr-1 xs:pr-2 ">
+      <Link to="/resume/" className="text-3xl xs:text-2xl font-extrabold tracking-tight text-white hover:underline hover:ml-4">
+        <h2 className="py-24 xs:py-12 lg:text-left sm:text-center xs:text-center xs:mx-auto">See my resume &rarr;</h2>
+      </Link>
+    </div>
+  </div>
+  
+)
+
