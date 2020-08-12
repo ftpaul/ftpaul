fc3586786ec4f5aed53835031d5e83c6e169469d
Paulo Teixeira <ftpaul@gmail.com>
2020-08-12 11:58:47 +0100

    mobile improvements

--- src/pages/index.js
+++ src/pages/index.js
@@ -56,7 +56,7 @@ const AboutPage = ({ data }) => { return (
             <div class="flex-shrink-0">
               <div class="flex items-center justify-center h-12 w-12 rounded-md bg-yellow-600 text-white">
                 <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
                 </svg>
               </div>
             </div>
@@ -73,7 +73,7 @@ const AboutPage = ({ data }) => { return (
             <div class="flex-shrink-0">
               <div class="flex items-center justify-center h-12 w-12 rounded-md bg-yellow-600 text-white">
                 <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3" />
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3" />
                 </svg>
               </div>
             </div>
@@ -125,13 +125,13 @@ const AboutPage = ({ data }) => { return (
 </div>
   </ContentColumn>
 
-  <div className="flex flex-wrap w-full mx-auto pt-8 xs:pt-0 px-2 bg-yellow-600">
-    <div className="w-1/2 lg:w-1/2 sm:w-full xs:w-full pt-4 pr-1 xs:pr-2 ">
+  <div className="flex xs:flex-col-reverse flex-wrap w-full mx-auto pt-8 xs:pt-0 px-2 bg-yellow-600">
+    <div className="w-1/2 lg:w-1/2 sm:1/2 xs:w-full pt-4 pr-1 xs:pr-2 ">
       <img src="/paulo-teixeira-resume-screenshot.png" alt="Paulo Teixeira | ftpaul.io" className="w-96 mx-auto z-20" />
     </div>
-    <div className="w-1/2 lg:w-1/2 sm:w-full xs:w-full pt-4 pr-1 xs:pr-2 ">
+    <div className="w-1/2 lg:w-1/2 sm:1/2 xs:w-full pt-4 pr-1 xs:pr-2 ">
       <Link to="/resume/" className="text-3xl xs:text-2xl font-extrabold tracking-tight text-white hover:underline hover:ml-4">
-        <h2 className="py-24" >See my resume &rarr;</h2>
+        <h2 className="py-24 xs:py-12 lg:text-left sm:text-center xs:text-center xs:mx-auto">See my resume &rarr;</h2>
       </Link>
     </div>
   </div>
@@ -187,20 +187,17 @@ const AboutPage = ({ data }) => { return (
 
  
   <div class="bg-yellow-600">
-  <div class="max-w-screen-xl mx-auto py-20 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
-    <h2 class="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
-      Ready to set up your game?
-      <br/>
-      <span class="text-white">Get in touch with me today.</span>
-    </h2>
-    <div class="mt-8 flex lg:flex-shrink-0 lg:mt-0">
-      {/* <div class="inline-flex rounded-md shadow">
-        <a href="#" class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-gray-300 hover:bg-gray-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">
-          Contact me
-        </a>
-      </div> */}
+    <div class="max-w-screen-xl flex flex-wrap mx-auto py-20 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+      <div className="w-4/6 xs:w-full">
+      <h2 class="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-3xl xs:font-bold xs:text-2xl sm:leading-10 flex-shrink">
+        Ready to set up your game?
+        <br/>
+        <span class="text-white">Get in touch with me today.</span>
+      </h2>
+      </div>
+    <div class="w-2/6 xs:w-full mt-8 sm:my-4 flex lg:flex-shrink-0 lg:mt-0">
       <div class="ml-3 xs:ml-0 inline-flex rounded-md shadow">
-        <a href="#" class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-yellow-600 bg-white hover:bg-yellow-600 hover:border-4 hover:border-white hover:text-white focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">
+        <a href="#" class="inline-flex font-bold items-center justify-center px-5 py-3 border border-transparent text-lg leading-6 rounded-md text-yellow-600 bg-white hover:bg-yellow-600 hover:border-4 hover:border-white hover:text-white focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">
           Get in touch
         </a>
       </div>
