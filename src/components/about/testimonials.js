f8809d8015a27cd254f6a37dd9dbf2f3ee5d7344
Paulo Teixeira <ftpaul@gmail.com>
2020-08-16 12:48:38 +0100

    small visual updates

--- src/components/about/testimonials.js
+++ src/components/about/testimonials.js
@@ -19,9 +19,9 @@ export default () => (
           
           <div class="w-1/2 lg:w-1/2 sm:w-1/2 xs:w-full mx-auto pt-1 mb-4 text-left pr-4 border-r-2 border-yellow-600 xs:border-r-0 xs:border-b-2 xs:pb-8">
               <svg xmlns="http://www.w3.org/2000/svg" className="absolute -ml-4 xs:-ml-1 mt-0 xs:-mt-1 fill-current text-yellow-600 h-4 w-4 z-0" viewBox="0 0 24 24"><path d="M13 14.725c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275zm-13 0c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275z"/></svg>
-              <p class="mt-2 text-xl leading-6 italic text-gray-400 z-10 relative">
+              <p class="mt-2 text-xl leading-6 italic font-light text-gray-400 z-10 relative">
                 Paulo is an ace team player and deeply cares about creating an environment in which everyone can excel. He frequently checks in with the people around him and does what is in his power to support them and remove roadblocks. </p>
-              <p class="mt-2 text-xl leading-6 italic text-gray-400">
+              <p class="mt-2 text-xl leading-6 italic font-light text-gray-400">
                 Finally, Paulo is the definition of zen. His calm and controlled nature is definitely something that affects people around him in the most positive way. His positive attitude is contagious.
               </p>
               <div className="text-base leading-6 font-medium text-gray-500 mt-4 text-left xs:ml-0 flex ">
@@ -33,11 +33,11 @@ export default () => (
             
           </div>
 
-          <div class="w-1/2 lg:w-1/2 sm:w-1/2 xs:w-full mx-auto pt-1 mb-4 pl-8 text-left xs:pl-0 xs:pt-8">
+          <div class="w-1/2 lg:w-1/2 sm:w-1/2 xs:w-full mx-auto pt-1 mb-4 pl-8 text-left xs:pl-0 xs:pt-8 xs:border-yellow-600 xs:border-r-0 xs:border-b-2 xs:pb-8">
               <svg xmlns="http://www.w3.org/2000/svg" className="absolute -ml-4 xs:-ml-1 mt-0 xs:-mt-1 fill-current text-yellow-600 h-4 w-4 z-0" viewBox="0 0 24 24"><path d="M13 14.725c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275zm-13 0c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275z"/></svg>
-              <p class="mt-2 text-xl leading-6 italic text-gray-400 z-10 relative">
+              <p class="mt-2 text-xl leading-6 italic font-light text-gray-400 z-10 relative">
               (...) He transitioned from marketing into product management really well, he has the product mindset of understanding the problem accurately and them moving to the solution. </p>
-              <p class="mt-2 text-xl leading-6 italic text-gray-400 z-10 relative">
+              <p class="mt-2 text-xl leading-6 italic font-light text-gray-400 z-10 relative">
                 Paulo adapts very fast to the changing conditions of the market, which often require last minute changes to on-going projects. These are never a problem but are seen as an opportunity to further improve our position.
                 </p>
               <div className="text-base leading-6 font-medium text-gray-500 mt-4 text-left xs:ml-0 flex ">
@@ -51,7 +51,7 @@ export default () => (
 
           <div class="w-1/2 lg:w-1/2 sm:w-1/2 xs:w-full mx-auto mt-12 text-left pr-4 border-r-2 border-yellow-600 xs:border-r-0 xs:border-b-2 xs:pb-8 ">
               <svg xmlns="http://www.w3.org/2000/svg" className="absolute -ml-4 xs:-ml-1 mt-0 xs:-mt-1 fill-current text-yellow-600 h-4 w-4 z-0" viewBox="0 0 24 24"><path d="M13 14.725c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275zm-13 0c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275z"/></svg>
-              <p class="mt-2 text-xl leading-6 italic text-gray-400 z-10 relative">
+              <p class="mt-2 text-xl leading-6 italic font-light text-gray-400 z-10 relative">
               Paulo is known for always asking the right questions, challenging anyone in the team. His curiosity is sincere and positive. He is dedicated to growth, experimentation and learning - a valuable asset in a fast paced organisation.
               </p>
               <div className="text-base leading-6 font-medium text-gray-500 mt-4 text-left xs:ml-0 flex  ">
@@ -65,8 +65,8 @@ export default () => (
 
           <div class="w-1/2 lg:w-1/2 sm:w-1/2 xs:w-full mx-auto mt-12 pl-8 text-left xs:pl-0 xs:mt-12 ">
               <svg xmlns="http://www.w3.org/2000/svg" className="absolute -ml-4 xs:-ml-1 mt-0 xs:-mt-1 fill-current text-yellow-600 h-4 w-4 z-0" viewBox="0 0 24 24"><path d="M13 14.725c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275zm-13 0c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275z"/></svg>
-              <p class="mt-2 text-xl leading-6 italic text-gray-400 z-10 relative">
-              Paulo’s unique personality traits are a must in any team — he’ll keep challenging every manager to seek excellence while granting a more humane group of people working together.</p>
+              <p class="mt-2 text-xl leading-6 italic font-light text-gray-400 z-10 relative">
+                Paulo’s unique personality traits are a must in any team — he’ll keep challenging every manager to seek excellence while granting a more humane group of people working together.</p>
               <div className="text-base leading-6 font-medium text-gray-500 mt-4 text-left xs:ml-0 flex ">
                 <img src="/about/mariana-silva.jpeg" alt="Mariana Silva" className="rounded-full w-12 h-12"/>
                 <p className="ml-2 text-gray-500 font-bold">Mariana Silva <br />
