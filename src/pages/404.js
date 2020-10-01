84c2fde7389fe6256371cbc2201481eb3b909da4
Paulo Teixeira <ftpaul@gmail.com>
2020-10-01 15:57:01 +0100

    404 small update

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,8 +8,13 @@ const IndexPage = ({ data }) => { return (
   <SEO />
 
   <Layout>
-    <h1 className="text-2xl text-bold text-gray-600">You just hit a road block <span role="img" aria-label="Construction">🚧</span></h1>
-    <h2 className="text-2xl text-bold text-gray-600">That sucks. Please go back <Link className="text-yellow-600 underline hover:text-yellow-400" to="/" >home</Link>.</h2>
+    <div className="text-center">
+      <h1 className="text-2xl text-bold text-gray-600">You just hit a road block <span role="img" aria-label="Construction">🚧</span></h1>
+      <h2 className="text-2xl text-bold text-gray-600">
+          That sucks! &nbsp;
+          <Link className="text-yellow-600 underline hover:text-yellow-400" to="/" >Please go back home</Link>.
+      </h2>
+    </div>
   </Layout>
   </>)
 }
