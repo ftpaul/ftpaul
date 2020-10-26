c315b631b1ca58a2891974247c02656c66846db0
Paulo Teixeira <ftpaul@gmail.com>
2020-10-26 12:49:54 +0000

    hidding shorter testimonials on mobile

--- src/components/about/testimonials.js
+++ src/components/about/testimonials.js
@@ -32,7 +32,7 @@ export default () => (
             
           </div>
 
-          <div className="w-1/2 lg:w-1/2 sm:w-1/2 xs:w-full mx-auto pt-1 mb-4 pl-8 text-left xs:pl-0 xs:pt-8 xs:border-yellow-600 xs:border-r-0 xs:border-b-2 xs:pb-8">
+          <div className="w-1/2 lg:w-1/2 sm:w-1/2 xs:w-full mx-auto pt-1 mb-4 pl-8 text-left xs:pl-0 xs:pt-8 xs:border-yellow-600 xs:border-0 xs:pb-0 xs:mb-0">
               <svg xmlns="http://www.w3.org/2000/svg" className="absolute -ml-4 xs:-ml-1 mt-0 xs:-mt-1 fill-current text-yellow-600 h-4 w-4 z-0" viewBox="0 0 24 24"><path d="M13 14.725c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275zm-13 0c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275z"/></svg>
               <p className="mt-2 text-xl leading-6 italic font-light text-gray-400 z-10 relative">
               (...) He transitioned from marketing into product management really well, he has the product mindset of understanding the problem accurately and them moving to the solution. </p>
@@ -48,7 +48,7 @@ export default () => (
             
           </div>
 
-          <div className="w-1/2 lg:w-1/2 sm:w-1/2 xs:w-full mx-auto mt-12 text-left pr-4 border-r-2 border-yellow-600 xs:border-r-0 xs:border-b-2 xs:pb-8 ">
+          <div className="w-1/2 lg:w-1/2 sm:w-1/2 xs:w-full mx-auto mt-12 text-left pr-4 border-r-2 border-yellow-600 xs:border-r-0 xs:border-b-2 xs:pb-8 xs:hidden">
               <svg xmlns="http://www.w3.org/2000/svg" className="absolute -ml-4 xs:-ml-1 mt-0 xs:-mt-1 fill-current text-yellow-600 h-4 w-4 z-0" viewBox="0 0 24 24"><path d="M13 14.725c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275zm-13 0c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275z"/></svg>
               <p className="mt-2 text-xl leading-6 italic font-light text-gray-400 z-10 relative">
               Paulo is known for always asking the right questions, challenging anyone in the team. His curiosity is sincere and positive. He is dedicated to growth, experimentation and learning - a valuable asset in a fast paced organisation.
@@ -62,7 +62,7 @@ export default () => (
             
           </div>
 
-          <div className="w-1/2 lg:w-1/2 sm:w-1/2 xs:w-full mx-auto mt-12 pl-8 text-left xs:pl-0 xs:mt-12 ">
+          <div className="w-1/2 lg:w-1/2 sm:w-1/2 xs:w-full mx-auto mt-12 pl-8 text-left xs:pl-0 xs:mt-12 xs:hidden">
               <svg xmlns="http://www.w3.org/2000/svg" className="absolute -ml-4 xs:-ml-1 mt-0 xs:-mt-1 fill-current text-yellow-600 h-4 w-4 z-0" viewBox="0 0 24 24"><path d="M13 14.725c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275zm-13 0c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275z"/></svg>
               <p className="mt-2 text-xl leading-6 italic font-light text-gray-400 z-10 relative">
                 Paulo’s unique personality traits are a must in any team — he’ll keep challenging every manager to seek excellence while granting a more humane group of people working together.</p>
