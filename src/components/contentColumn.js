e5702f47c868bfb9407992e8898a3c449b3e2606
Paulo Teixeira <ftpaul@gmail.com>
2020-11-07 20:24:51 +0000

    2nd iteration on books page

--- src/components/contentColumn.js
+++ src/components/contentColumn.js
@@ -2,9 +2,9 @@ import React from "react"
 
 export default ({ children, shorterColumn, largerColumn }) => (
 
-  <div className={shorterColumn ? `max-w-screen-md flex flex-wrap mx-auto pt-8 xs:pt-0 px-2` : 
-    (largerColumn ? `max-w-screen-xl flex flex-wrap mx-auto pt-8 xs:pt-0 px-2` : 
-    `max-w-screen-lg flex flex-wrap mx-auto pt-8 xs:pt-0 px-2`)}>
+  <div className={shorterColumn ? `max-w-screen-md mx-auto pt-8 xs:pt-0 px-2` : 
+    (largerColumn ? `max-w-screen-xl mx-auto pt-8 xs:pt-0 px-2` : 
+    `max-w-screen-lg mx-auto pt-8 xs:pt-0 px-2`)}>
     
      {children}
     
