2fa0d697117fd4050dfb6737b7fa6a0edde2104b
Paulo Teixeira <ftpaul@gmail.com>
2020-11-22 11:35:48 +0000

    added illustrations link to footer

--- src/components/navigation/footer.js
+++ src/components/navigation/footer.js
@@ -6,8 +6,8 @@ export default ( ) => (
 
   <div className="flex flex-wrap max-w-screen-lg mx-auto py-8 px-4 justify-center md:justify-center sm:justify-start xs:justify-start">
     <p className="text-sm">Made by Paulo Teixeira, a Product Manager based in Lisbon. This website uses&nbsp;
-      <a href="https://gatsbyjs.org" className="text-yellow-600 underline hover:text-yellow-400">Gatsby</a> and&nbsp;
-      <a href="https://tailwindcss.com/" className="text-yellow-600 underline hover:text-yellow-400">Tailwind</a>.
+      <a href="https://gatsbyjs.org" className="text-yellow-600 underline hover:text-yellow-400">Gatsby</a>. Illustrations by&nbsp;
+      <a href="https://icons8.com/" className="text-yellow-600 underline hover:text-yellow-400">icons8.com</a>.  
     </p>
   </div>
   
