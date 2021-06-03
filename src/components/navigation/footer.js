df8449fc54dbad160ec373f73eeecbc280bce957
ftpaul <ftpaul@gmail.com>
2021-06-03 23:48:39 +0200

    added new images, retouched footer

--- src/components/navigation/footer.js
+++ src/components/navigation/footer.js
@@ -1,13 +1,11 @@
 import React from "react"
-//import ContentColumn from "./contentColumn"
+import { Link } from "gatsby"
 
 export default ( ) => (
 
 
   <div className="flex flex-wrap max-w-screen-lg mx-auto py-8 px-4 justify-center md:justify-center sm:justify-start xs:justify-start">
-    <p className="text-sm">Made by Paulo Teixeira, a Product Manager based in Lisbon. Illustrations by&nbsp;
-      <a href="https://icons8.com/" className="text-yellow-600 underline hover:text-yellow-400">icons8.com</a>.  
-    </p>
+    <p className="text-sm">Made by Paulo Teixeira, a Product Manager based in Berlin. Check the <Link className="text-yellow-600 underline hover:text-yellow-400" to="/backlog/">Backlog</Link> for future improvements.</p>
   </div>
   
 )
