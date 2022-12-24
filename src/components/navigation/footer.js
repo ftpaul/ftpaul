8ba91978c68468ac8e0a4878916b5a915e72bcfb
Paulo Teixeira <ftpaul@gmail.com>
2022-12-24 11:52:48 +0000

    added AL to CV, updated CV to allow empty experience

--- src/components/navigation/footer.js
+++ src/components/navigation/footer.js
@@ -6,7 +6,6 @@ const Footer = () => (
 
   <div className="flex flex-wrap max-w-screen-lg mx-auto py-8 px-4 justify-center md:justify-center sm:justify-start xs:justify-start border-t border-gray-100">
 
-    {/* <p className="text-sm">Made by Paulo Teixeira, a Product Manager based in Berlin. Check the <Link className="text-yellow-600 underline hover:text-yellow-400" to="/backlog/">Backlog</Link> for future improvements.</p> */}
     <p className="text-sm">Made by Paulo Teixeira. Based in Berlin.</p>
     
   </div>
