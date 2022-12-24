6a83628d773fecee80f76e6516d28f119023c4d0
Paulo Teixeira <ftpaul@gmail.com>
2022-12-24 17:20:53 +0000

    removing posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,43 +1,48 @@
-exports.createPages = async ({ actions, graphql, reporter }) => {
-  const { createPage } = actions
-  const blogPostTemplate = require.resolve(`./src/templates/post.js`)
-  const result = await graphql(`
-    {
-      allMarkdownRemark(
-        sort: { order: DESC, fields: [frontmatter___date] }
-        filter: {fileAbsolutePath: {regex: "/post/"}}
-      ) {
-        edges {
-          node {
-            frontmatter {
-              slug
-            }
-          }
-        }
-      }
-    }
-  `)
-  // Handle errors
-  if (result.errors) {
-    reporter.panicOnBuild(`Error while running GraphQL query.`)
-    return
-  }
-  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-    createPage({
-      path: `/writing`+node.frontmatter.slug,
-      component: blogPostTemplate,
-      context: {
-        // additional data can be passed via context
-        slug: node.frontmatter.slug,
-      },
-    })
-  })
-}
-
 // moment.js comes with gatsby
-const moment = require('moment');
+//const moment = require('moment');
+
+// exports.createPages = async ({ actions, graphql, reporter }) => {
+//   const { createPage } = actions
+//   const blogPostTemplate = require.resolve(`./src/templates/post.js`)
+//   const result = await graphql(`
+//     {
+//       allMarkdownRemark(
+//         sort: { order: DESC, fields: [frontmatter___date] }
+//         filter: {fileAbsolutePath: {regex: "/post/"}}
+//       ) {
+//         edges {
+//           node {
+//             frontmatter {
+//               slug
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `)
+//   // Handle errors
+//   if (result.errors) {
+//     reporter.panicOnBuild(`Error while running GraphQL query.`)
+//     return
+//   }
+//   result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+//     createPage({
+//       path: `/writing`+node.frontmatter.slug,
+//       component: blogPostTemplate,
+//       context: {
+//         // additional data can be passed via context
+//         slug: node.frontmatter.slug,
+//       },
+//     })
+//   })
+// }
+
+
 
 exports.onCreateNode = ({ node, actions }) => {
+
+  const moment = require('moment');
+  
   const { createNodeField } = actions
 
   // https://stackoverflow.com/questions/54473828/how-to-query-date-range-in-gatsby-graphql
@@ -46,19 +51,6 @@ exports.onCreateNode = ({ node, actions }) => {
     const time_read = node.time_read
     //const readDay = node.readDay
     const time_added = node.time_added
-
-   
-    // createNodeField({
-    //   node,
-    //   name: 'time_updated',
-    //   value: moment.unix(time_updated).format(),
-    // })
-
-    // createNodeField({
-    //   node,
-    //   name: 'time_read',
-    //   value: moment.unix(time_read).format(),
-    // })
 
     createNodeField({
       node,
@@ -71,12 +63,6 @@ exports.onCreateNode = ({ node, actions }) => {
       name: 'year_read',
       value: moment.unix(time_read).format(`YYYY`),
     })
-
-    // createNodeField({
-    //   node,
-    //   name: 'readDay',
-    //   value: moment.unix(readDay).format(),
-    // })
 
     createNodeField({
       node,
