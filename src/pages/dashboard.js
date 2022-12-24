d982af51626a0ea579016fed2ad5c93a45b211a7
Paulo Teixeira <ftpaul@gmail.com>
2022-12-24 14:48:32 +0000

    added pocket data into the project

---
+++ src/pages/dashboard.js
@@ -0,0 +1,78 @@
+import React from "react"
+import { graphql } from "gatsby"
+import 'chartkick/chart.js'
+import { ColumnChart } from 'react-chartkick'
+// import Layout from '../components/layout'
+// import SEO from '../components/SEO'
+import ContentColumn from '../components/contentColumn'
+
+const DashboardPage = ({ data }) => {
+  
+
+  const queryReadArticles = data.readArticles.group
+  const queryAddedArticles = data.addedArticles.group
+  
+
+
+  var finalDataRead = []
+  queryReadArticles.map(element => (
+    finalDataRead.push([element.fieldValue, element.totalCount])
+  ))
+
+  var finalDataAdded = []
+  queryAddedArticles.map(element => (
+    finalDataAdded.push([element.fieldValue, element.totalCount])
+  ))
+
+  
+  return (
+    <ContentColumn>
+    {/* <SEO title="Home" /> */}
+    <h2>Articles Read</h2>
+    <ColumnChart 
+      data={finalDataRead.slice(-40)} 
+      colors={["#EF4155"]}
+      xtitle="Months" 
+      ytitle="Articles Read" 
+    />
+
+    <h2>Articles Added</h2>
+    <ColumnChart 
+        data={finalDataAdded.slice(-40)} 
+        colors={["#EF4155"]}
+        xtitle="Months" 
+        ytitle="Articles Added" 
+      />
+
+<pre>{JSON.stringify(data.readArticles, null, 4)}</pre>
+  </ContentColumn>
+
+)}
+
+// https://github.com/conradj/gatsby-source-pocket/issues/2 dates are UNIX type
+// https://chartkick.com/react
+
+export const query = graphql`
+  {
+    readArticles: allPocketArticle(sort: {fields: fields___month_read},filter: {fields: {month_read: {nin: "Invalid date"}}}) {
+      group(field: fields___month_read) {
+        totalCount
+        fieldValue
+      }
+    }
+    addedArticles: allPocketArticle(sort: {fields: fields___month_read},filter: {fields: {month_read: {nin: "Invalid date"}}}) {
+      group(field: fields___month_added) {
+        totalCount
+        fieldValue
+      }
+    }
+  }
+`
+// addedArticles: allPocketArticle(sort: {fields: fields___month_added}) {
+//   group(field: fields___month_added) {
+//     totalCount
+//     fieldValue
+//   }
+// }
+
+export default DashboardPage
