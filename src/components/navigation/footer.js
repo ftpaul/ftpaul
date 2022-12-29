f2bdf3189eaab6c1ebeecf7ee321639e243b8480
Paulo Teixeira <ftpaul@gmail.com>
2022-12-29 16:01:27 +0000

    added flag (test commit)

--- src/components/navigation/footer.js
+++ src/components/navigation/footer.js
@@ -6,7 +6,7 @@ const Footer = () => (
 
   <div className="flex flex-wrap max-w-screen-lg mx-auto py-8 px-4 justify-center md:justify-center sm:justify-start xs:justify-start border-t border-gray-100">
 
-    <p className="text-sm">Made by Paulo Teixeira. Based in Berlin.</p>
+    <p className="text-sm">Made by Paulo Teixeira. Based in Berlin 🇩🇪</p>
     
   </div>
   
