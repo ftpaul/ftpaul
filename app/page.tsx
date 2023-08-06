30229fc5876faf991fd7837fa08ae2d86a5b537d
Paulo Teixeira <ftpaul@gmail.com>
2023-08-06 22:40:59 +0200

    homepage iteration

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
-export default function IndexPage() {
-    return "<div>home</div>";
+
+const getData = async () =>  {
+  const data = await fetch("https://www.reddit.com/.json")
+  return data.json()
+}
+
+export default async function IndexPage() {
+
+  const data = await getData()
+  const post = data.data.children[0].data.title
+
+    return (
+      <h1>{post}</h1>
+    )
   }
