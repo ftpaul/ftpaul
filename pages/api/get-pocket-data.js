464411598553bd172ae358a2638b4f2a0dba0f46
Paulo Teixeira <ftpaul@gmail.com>
2023-08-24 12:58:08 +0200

    improved data logic

--- pages/api/get-pocket-data.js
+++ pages/api/get-pocket-data.js
@@ -1,9 +1,8 @@
 
 export default async (req, res) => {
     try {
-      const response = await fetch(`https://getpocket.com/v3/get?access_token=${process.env.NEXT_PUBLIC_POCKET_ACCESS_TOKEN}&consumer_key=${process.env.NEXT_PUBLIC_POCKET_CONSUMER_KEY}&since=1577833200`);
+      const response = await fetch(`https://getpocket.com/v3/get?access_token=${process.env.NEXT_PUBLIC_POCKET_ACCESS_TOKEN}&consumer_key=${process.env.NEXT_PUBLIC_POCKET_CONSUMER_KEY}&since=1622498400&detail_type=simple`);
       const data = await response.json();
-      console.log(data);
       res.status(200).json(data);
   } catch (error) {
       res.status(500).json({ error: 'Failed to fetch data' });
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,23 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import { LineChart, PieChart, ColumnChart } from 'react-chartkick'
 import 'chartkick/chart.js'
+import { Console } from 'console';
 
 
 
-
-function getReadArticlesByMonth(articles) {
+function getGroupArticlesByMonth(articles, groupBy) {
   // Filter articles with time_read different than 0
   const readArticles = articles
-      .filter(article => article.time_read !== "0" &&
-          article.time_read !== undefined);
+      .filter(article => article[groupBy] !== "0" &&
+          article[groupBy] !== undefined &&
+          article[groupBy] > 1622498400); // avoid edge cases with old dates
       
   // Group articles by month
   const groupedByMonth = readArticles.reduce((acc, article) => {
-      console.log(article.time_read)
-    const date = new Date(Number(article.time_read) * 1000);  // Convert UNIX timestamp to JavaScript Date
+    //console.log(article[groupBy])
+    const date = new Date(Number(article[groupBy]) * 1000);  // Convert UNIX timestamp to JavaScript Date
     const month = String(date.getMonth() + 1).padStart(2, '0'); // Ensure month has two digits
     const year = date.getFullYear();
-    const monthYearKey = `${month}-${year}`;  // e.g., "05-2021" for May 2021
+    const monthYearKey = `${year}-${month}-1`;  // e.g., "05-2021" for May 2021
 
     // Initialize the month-year key if it doesn't exist
     if (!acc[monthYearKey]) {
@@ -27,75 +28,75 @@ function getReadArticlesByMonth(articles) {
     // Increment the count for the month-year key
     acc[monthYearKey]++;
 
-
-
-    
     return acc;
   }, {});
 
   return groupedByMonth;
 }
 
-function orderByMonth(readArticlesByMonth) {
-  Object.entries(readArticlesByMonth)
-  .sort((a, b) => {
-  const [monthA, yearA] = a[0].split('-').map(Number);
-  const [monthB, yearB] = b[0].split('-').map(Number);
-
-  // Compare by year first, then by month
-  return yearA - yearB || monthA - monthB;
-  })
-  .reduce((acc, [monthYearKey, count]) => {
-  acc[monthYearKey] = count;
-  return acc;
-  }, {});
-  return readArticlesByMonth;
-}
 
 export default function DashboardPage() {
-    const [dataList, setDataList] = useState([]);
-    const [orderedByMonth, setOrderedByMonth] = useState({});
+    
+  const [dataList, setDataList] = useState([]);
+  const [readArticlesByMonth, setReadArticlesByMonth] = useState({});
+  const [addedArticlesByMonth, setAddedArticlesByMonth] = useState({});
+  var data = []
 
-    useEffect(() => {
-      async function fetchData() {
-          try {
-              const response = await fetch('/api/get-pocket-data');
-              
-              if (!response.ok) {
-                  throw new Error('Network response was not ok');
-              }
-              
-              const result = await response.json();
-              
-              setDataList(Object.values(result.list));
-              
-              const readArticlesByMonth = getReadArticlesByMonth(Object.values(result.list));
-              const orderedData = orderByMonth(readArticlesByMonth);
+  useEffect(() => {
+    async function fetchData() {
+        try {
+            const response = await fetch('/api/get-pocket-data');
+            
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            
+            const result = await response.json();
+            
+            setDataList(Object.values(result.list));
+            
+            const readArticlesByMonth = getGroupArticlesByMonth(Object.values(result.list), "time_read");
+
+            const addedArticlesByMonth = getGroupArticlesByMonth(Object.values(result.list), "time_added");
+            
+            setAddedArticlesByMonth(addedArticlesByMonth);
+            setReadArticlesByMonth(readArticlesByMonth);
+            
+            
+        } catch (error) {
+            console.error("There was a problem fetching the data:", error);
+        }
+    }
+
+    fetchData();
+}, []);
   
-              setOrderedByMonth(orderedData);
-          } catch (error) {
-              console.error("There was a problem fetching the data:", error);
-          }
-      }
-  
-      fetchData();
-  }, []);
 
-    // ... rest of the component
+  return (
+      <>
+          <PieChart data={[["Blueberry", 44], ["Strawberry", 23]]} id="something" /> 
 
-   
-
-    return (
-        <>
-            <PieChart data={[["Blueberry", 44], ["Strawberry", 23]]} id="something" /> 
-
-            <LineChart 
-                        data={orderedByMonth} 
-                        colors={["#EF4155"]}
-                        xtitle="Months" 
-                        ytitle="Articles Added" 
-                        id="loading"
-                    />
-        </>
-    );
+          <LineChart 
+                      data={[{name: "read articles", data: readArticlesByMonth}, {name: "added articles", data: addedArticlesByMonth}]} 
+                      colors={["#EF4155"]}
+                      xtitle="Months" 
+                      ytitle="Articles Added" 
+                      id="loading"
+                  />
+                  <LineChart 
+                      data={readArticlesByMonth} 
+                      colors={["#EF4155"]}
+                      xtitle="Months" 
+                      ytitle="Articles Read" 
+                      id="loading"
+                  />
+                  <LineChart 
+                      data={addedArticlesByMonth} 
+                      colors={["#EF4155"]}
+                      xtitle="Months" 
+                      ytitle="Articles Added" 
+                      id="loading"
+                  />
+      </>
+  );
 }
