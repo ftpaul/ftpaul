2b29fb975a6fb809650daf9cebdfd850fc1bb70c
Paulo Teixeira <ftpaul@gmail.com>
2023-10-16 23:23:17 +0200

    feat: added footer content and components

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,13 +1,16 @@
 // import React from "react";
 // import Seo from '../components/seo';
-// import Footer from '../components/resume/footerCTA';
-// import ExperienceBlock from '../components/resume/experienceBlock';
+import ExperienceBlock from '../../components/resume/ExperienceBlock';
+import ResumeFooter from '../../components/resume/DownloadFooter';
 // import Navigation from '../components/navigation/navigation';
 // import Image from 'next/image';
-// import { ResumeDataType } from '../types'; 
+// import { contentType } from '../types'; 
+import '../../styles/resume.css'
+
+import content from '../../content/resume.json'
 
 
-//const ResumePage: React.FC<ResumeProps> = ({ resumeData }) => {
+//const ResumePage: React.FC<ResumeProps> = ({ content }) => {
   export default async function ResumePage() {
 
   const seo = {
@@ -25,9 +28,7 @@
        ... 
       <Image src="/assets/images/ezgif-5-bea54725e5.jpeg" alt="Paulo Teixeira | ftpaul.io" width={160} height={160} className="w-40 h-40 xs:w-20 xs:h-20 mt-4 z-20 rounded-full" /       ... 
   
-   <Navigation simplified={false} /> 
-
- <Navigation  /> */}
+   */}
 
   <div className="letter p-8 mx-auto mb-24 mt-6 bg-white relative max-w-screen-lg w-11/12">
 
@@ -37,10 +38,10 @@
             
           <h1 className="text-5xl xs:text-3xl font-bold text-gray-600 leading-none pt-4 pb-0 mb-0">
           <button className="focus:outline-none outline-none cursor-default hover:cursor-default" >
-                resumeData.name<span className="font-black text-6xl xs:text-3xl text-yellow-600">.</span>
+                {content.name}<span className="font-black text-6xl xs:text-3xl text-yellow-600">.</span>
             </button> 
           </h1>
-          <h2 className="text-4xl xs:text-2xl font-normal text-gray-300 pb-4">resumeData.title</h2>
+          <h2 className="text-4xl xs:text-2xl font-normal text-gray-300 pb-4">{content.title}</h2>
 
       </div>
 
@@ -58,14 +59,13 @@
     <div className="w-4/6 xs:w-full pt-4 pr-1 xs:pr-2 ">
 
       <h4 className="uppercase text-xs text-gray-300 tracking-widest">Summary</h4>
-      <p className="text-lg text-gray-500 pb-6">resumeData.summary</p>
+      <p className="text-lg text-gray-500 pb-6">{content.summary}</p>
 
       <h4 className="uppercase text-xs text-gray-300 tracking-widest w-full">Experience</h4>
 
-      {/* {resumeData.experience.map((job) => (
-        // <ExperienceBlock job={job} key={job.end_date} />
-        <p>one</p>
-      ))} */}
+      {content.experience.map((job) => (
+        <ExperienceBlock job={job} key={job.end_date} />
+      ))}
       
     </div>
     
@@ -75,45 +75,45 @@
     <div className="w-2/6 xs:w-full p-4 xs:pl-0 " >
 
       <div className="pt-6 pb-8 xs:hidden">
-        <p className="text-base font-bold text-gray-500">Based in resumeData.details.location.city <span role="img" aria-label="resumeData.details.location.country"  ></span></p>
+        <p className="text-base font-bold text-gray-500">Based in {content.details.location.city} <span role="img" aria-label="content.details.location.country"  ></span></p>
       </div>
 
       <div className="pb-8">
         <p className="text-base font-bold text-gray-500">Languages</p>
-        {/* {resumeData.details.languages.map(language => (
+        {content.languages.map(language => (
           <p className="text-base text-gray-500 pb-0" key={language}>{language}</p>
-        ))} */}
+        ))} 
       </div>
 
       <div className="pb-2 xs:hidden">
         <p className="text-base pb-2 font-bold text-gray-500">Skills &amp; Competencies</p>
-        {/* {resumeData.details.industry_knowledge.map(group => (
+        {content.industry_knowledge.map(group => (
           <div className="pb-3" key={group.name}>
             <p className="text-base capitalize text-gray-500 pb-0" >{group.name}</p>
-            {/* {group.individuals.map(individuals => (
+             {group.individuals.map(individuals => (
               <p className="text-base capitalize text-gray-500" key={individuals}> - {individuals}</p>
             ))} 
           </div>
-        )) */}
+        ))} 
       </div>
 
 
       <div className="pb-8">
         <p className="text-base font-bold text-gray-500">Projects</p>
-        {/* {resumeData.details.projects.map(project => (
+        {content.projects.map(project => (
           <div key={project.name} >
           <p className="text-base text-gray-500 pb-0 underline">
-            <a href={project.link} alt={project.name} className="text-yellow-600 underline hover:text-yellow-400">
+            <a href={project.link} className="text-yellow-600 underline hover:text-yellow-400">
             {project.name}</a>
             </p>
           <p className="text-base text-gray-500 pb-0">{project.description}</p>
           </div>
-        ))} */}
+        ))} 
       </div>
 
       <div className="pb-8">
         <p className="text-base font-bold text-gray-500">Random facts</p>
-        {/* {resumeData.details.random.map(fact => (
+        {content.random.map(fact => (
           <div key={fact.link}>
           <p className="text-base text-gray-500 pb-0" >
             {((fact.link.length > 0) ? (<a className="text-yellow-600 underline hover:text-yellow-400" href={fact.link}>{fact.description}</a>) :
@@ -123,7 +123,7 @@
             
           </p>
           </div>
-        ))} */}
+        ))} 
       </div>
 
     </div>
@@ -134,13 +134,9 @@
     <div className="py-1">
 
     </div>
-    {/* <Footer /> */}
+    <ResumeFooter />
     </>
   )
 }
 
-{ /*
- export async function getStaticProps() 
-  // Fetch your data here, for example:
-  //const data = await fetchData(); // Assume fetchData is a function 
-*/}
+
