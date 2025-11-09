2eeeaa5aaa87efd6d7a390280f6884568bc3d2fd
ftpaul <ftpaul@gmail.com>
2025-11-09 12:36:11 +0100

    header/logo image updated

--- components/navigation/Logo.tsx
+++ components/navigation/Logo.tsx
@@ -5,7 +5,7 @@ const Logo = () => (
     <Link href="/">
         {/* <h1 className="text-5xl font-bold text-gray-600 pb-4"><span className="font-black text-6xl text-green-600">.</span></h1> */}
         <Image 
-            src="/images/paulo-teixeira-main-image.jpeg" 
+            src="/images/paulo-teixeira-image-image.png" 
             alt="Paulo Teixeira" 
             className="w-12 max-h-12 mb-6 p-0 xs:p-4 rounded-full"
             width="64" 
