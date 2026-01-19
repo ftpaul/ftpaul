ccd0bba0c29620bb818ad84e2a6581d20d93e415
ftpaul <ftpaul@gmail.com>
2026-01-19 22:26:07 +0100

    added favicon

--- app/head.tsx
+++ app/head.tsx
@@ -3,6 +3,11 @@ export default function Head() {
       <>
         <title>Something using NextJS</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link 
+          rel="icon" 
+          href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><rect width='100' height='100' fill='%230a3c30'/></svg>"
+          type="image/svg+xml"
+        />
       </>
     );
   }
