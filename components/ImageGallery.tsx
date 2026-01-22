c032c4b5d4336f9f08e046dc685234de4db70567
ftpaul <ftpaul@gmail.com>
2026-01-22 17:23:57 +0100

    fixing overflow hidden

--- components/ImageGallery.tsx
+++ components/ImageGallery.tsx
@@ -26,11 +26,11 @@ const ImageGallery = () => {
   
 
   return (
-    <div className="flex justify-center items-center  space-x-12 xs:space-x-3 py-10">
+    <div className="flex justify-center items-center overflow-hidden space-x-12 xs:space-x-3 py-10">
       {shuffledImages.map((image, index) => (
         <div
           key={index}
-          className={`relative w-64 max-h-64  z-10 rounded-lg overflow-hidden shadow-lg transform ${
+          className={`relative w-64 max-h-64  z-10 rounded-lg shadow-lg transform ${
             index % 2 === 0 ? 'rotate-2' : '-rotate-2'
           } transition-transform duration-300 hover:scale-105
         ${index == 0 || index == (shuffledImages.length-1)? "xs:hidden" : " -translate-x-2 xs:w-1/3"} `}
